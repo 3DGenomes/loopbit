263e8da419d1d41a43c7117c9373300ca7ef85fc
Francois Serra <serra.francois@gmail.com>
2020-06-16 23:09:18 +0200

    faster cleaner scan

--- loopbit/scan.py
+++ loopbit/scan.py
@@ -4,17 +4,15 @@ def scan_chromosome(min_dist_reso, max_dist_reso, chromosome, bam_dict, size, st
     res = []
     for dist in range(min_dist_reso, max_dist_reso, step):
         for bstart in range(start_bin, end_bin, step):
-            start1, end1 = bstart, bstart + size
-            start2, end2 = end1 + (dist + 1), (end1 + (dist + 1)) + size
+            start1 = bstart
+            end1   = start1 + size
+            start2 = end1 + dist + 1
+            end2   = start2 + size
             if end2 <= end_bin:
-                mean1, mean2 = (start1 + end1) / 2, (start2 + end2) / 2
-                label = chromosome+'_'+str(int(mean1))+'_'+str(int(mean2))
-                matrix = np.zeros((size, size))
-                for x, p1 in enumerate(range(start1, end1)):
-                    for y, p2 in enumerate(range(start2, end2)):
-                        matrix[x,y] = bam_dict[(p1, p2)]
-                if np.count_nonzero(matrix) != 0:
-                    vector = matrix.flatten()
-                    prediction = model.predict([vector.reshape(-1, size, size, 1)])
-                    res.append((label, prediction))
+                mean1, mean2 = (start1 + end1) // 2, (start2 + end2) // 2
+                label = '{}_{}_{}'.format(chromosome, mean1, mean2)
+                vector = np.array([bam_dict[(p1, p2)] for p1 in range(start1, end1)
+                                   for p2 in range(start2, end2)])
+                prediction = model.predict([vector.reshape(-1, size, size, 1)])
+                res.append((label, prediction))
     return res
