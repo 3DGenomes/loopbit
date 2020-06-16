6c857e83cad697ebcd92f423cb7f7a6d0f83bdae
Francois Serra <serra.francois@gmail.com>
2020-06-16 23:15:47 +0200

    scan: get rid of numpy, simpler

--- loopbit/scan.py
+++ loopbit/scan.py
@@ -1,18 +1,24 @@
-import numpy as np
 
-def scan_chromosome(min_dist_reso, max_dist_reso, chromosome, bam_dict, size, step, start_bin, end_bin, model):
+def scan_chromosome(min_dist_reso, max_dist_reso, chromosome,
+                    bam_dict, size, step, start_bin, end_bin, model):
     res = []
+    # distance to diagonal
     for dist in range(min_dist_reso, max_dist_reso, step):
         for bstart in range(start_bin, end_bin, step):
             start1 = bstart
             end1   = start1 + size
             start2 = end1 + dist + 1
             end2   = start2 + size
-            if end2 <= end_bin:
-                mean1, mean2 = (start1 + end1) // 2, (start2 + end2) // 2
-                label = '{}_{}_{}'.format(chromosome, mean1, mean2)
-                vector = np.array([bam_dict[(p1, p2)] for p1 in range(start1, end1)
-                                   for p2 in range(start2, end2)])
-                prediction = model.predict([vector.reshape(-1, size, size, 1)])
-                res.append((label, prediction))
+            # avoid falling outside wanted region
+            if end2 > end_bin:
+                continue
+            # midpoint of the matrix used as label
+            mean1, mean2 = (start1 + end1) // 2, (start2 + end2) // 2
+            label = '{}_{}_{}'.format(chromosome, mean1, mean2)
+            # extract submatrix from Hi-C, as a vector
+            matrix = [[[[bam_dict[(p1, p2)]] for p2 in range(start2, end2)]
+                       for p1 in range(start1, end1)]]
+            # predict if the matrix adjusts to the model
+            prediction = model.predict([matrix])
+            res.append((label, prediction))
     return res
